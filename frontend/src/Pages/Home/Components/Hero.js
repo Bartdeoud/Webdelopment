e56497064bbdbe78ba533008f1f0daa62a796b49
Kikkerslijm410 <Lucasclavel356@gmail.com>
2022-12-23 12:27:55 +0100

    page fix 4

--- frontend/src/Pages/Home/Components/Hero.js
+++ frontend/src/Pages/Home/Components/Hero.js
@@ -3,8 +3,8 @@ import React from "react";
 const Hero = () => {
     return (
         <section className="hero">
-                <div className="background-image" style={{backgroundImage: 'url(Assets/image/Curtain2.jpg)'}} />
-                <a href="/Tickets" className="btn">Bestel hier uw Tickets!</a>
+                <div className="background-image" style={{backgroundImage: 'url(assets/image/Curtain2.jpg)'}} />
+                <a href="/Programma" className="btn">Bestel hier uw Tickets!</a>
         </section>
     );
 }
