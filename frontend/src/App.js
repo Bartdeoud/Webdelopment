8e191f885409addf3717aae80474af33f653c6f8
Kikkerslijm410 <Lucasclavel356@gmail.com>
2022-12-23 12:04:42 +0100

    page fix 2

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -23,29 +23,57 @@
 // export default App;
 
 import React from "react";
-import { Routes, Route, BrowserRouter } from "react-router-dom";
-import Home from "./Pages/Home/Home";
-import About from "./Pages/OverOns/OverOns";
-import Contact from "./Pages/Contact/Contact";
-import Programma from "./Pages/Programma/Programma";
-import Accesibility from "./Pages/Accesibility/Accesibility";
-import Voorwaarden from "./Pages/Voorwaarden/Voorwaarden";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
-class App extends React.Component {
-  render() {
-    return (
-      <BrowserRouter>
-          <Routes>
-            <Route exact path="/" element={<Home />} />
-            <Route path="/Overons" element={<About />} />
-            <Route path="/Contact" element={<Contact />} />
-            <Route path="/Programma" element={<Programma />} />
-            <Route path="/Accesibility" element={<Accesibility />} />
-            <Route path="/Voorwaarden" element={<Voorwaarden />} />
-          </Routes>
-      </BrowserRouter>
-    );
-  }
+export default function App() {
+  return (
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+            <li>
+              <Link to="/users">Users</Link>
+            </li>
+          </ul>
+        </nav>
+
+        {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+        <Switch>
+          <Route path="/about">
+            <About />
+          </Route>
+          <Route path="/users">
+            <Users />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+  );
 }
 
-export default App;
+function Home() {
+  return <h2>Home</h2>;
+}
+
+function About() {
+  return <h2>About</h2>;
+}
+
+function Users() {
+  return <h2>Users</h2>;
+}
