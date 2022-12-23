22b86bd98b1bc0d8cd4e45b2d91c20129943337a
Kikkerslijm410 <Lucasclavel356@gmail.com>
2022-12-23 12:12:33 +0100

    page not fixed

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,79 +1,23 @@
-// import './App.css';
+import './App.css';
 
-// import Header from './Pages/Shared/Header';
-// import Footer from './Pages/Shared/Footer';
-// import Title from './Pages/Shared/Titel';
-// import Main from './Main';
+import Header from './Pages/Shared/Header';
+import Footer from './Pages/Shared/Footer';
+import Title from './Pages/Shared/Titel';
+import Main from './Main';
 
-// function App() {
-//     return (
-//       <div>
-//         <meta charSet="utf-8" />
-//         <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
-//         <meta name="viewport" content="width=device-width, initial-scale=1" />
-//         <link rel="stylesheet" href="Assets/css/styles.css" />
-//         <Header />
-//         <Title />  
-//         <Main />
-//         <Footer />
-//       </div>
-//     );
-//   }
-
-// export default App;
-
-import React from "react";
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-  Link
-} from "react-router-dom";
-
-export default function App() {
-  return (
-    <Router>
+function App() {
+    return (
       <div>
-        <nav>
-          <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/about">About</Link>
-            </li>
-            <li>
-              <Link to="/users">Users</Link>
-            </li>
-          </ul>
-        </nav>
-
-        {/* A <Switch> looks through its children <Route>s and
-            renders the first one that matches the current URL. */}
-        <Routes>
-          <Route path="/about">
-            <About />
-          </Route>
-          <Route path="/users">
-            <Users />
-          </Route>
-          <Route path="/">
-            <Home />
-          </Route>
-        </Routes>
+        <meta charSet="utf-8" />
+        <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="stylesheet" href="Assets/css/styles.css" />
+        <Header />
+        <Title />  
+        <Main />
+        <Footer />
       </div>
-    </Router>
-  );
-}
+    );
+  }
 
-function Home() {
-  return <h2>Home</h2>;
-}
-
-function About() {
-  return <h2>About</h2>;
-}
-
-function Users() {
-  return <h2>Users</h2>;
-}
+export default App;
