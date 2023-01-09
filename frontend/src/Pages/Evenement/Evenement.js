f64d0a0838edc3cc66168746d2380330c22150d6
Profnoobstone <bartdeoud@gmail.com>
2023-01-09 13:45:53 +0100

    Small fix

--- frontend/src/Pages/Evenement/Evenement.js
+++ frontend/src/Pages/Evenement/Evenement.js
@@ -1,15 +1,10 @@
 import React, { Component } from "react";
-import Hero2 from "../Shared/Hero2";
 import EvenementValues from "./EvenementValues.js";
 
 class Programma extends Component {
     render () {
-
-
-
         return(
             <>
-                <Hero2 tekst="Evenement"/>
                 <EvenementValues/>
             </>
         );
