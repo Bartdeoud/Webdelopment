9423e0f66566b55e7014bb59bd91c48dc7f2b02a
HHS-ChristiaandeHaan <101116630+HHS-ChristiaandeHaan@users.noreply.github.com>
2023-01-09 16:44:35 +0100

    Changes to Header.js

--- frontend/src/Pages/Shared/Header.js
+++ frontend/src/Pages/Shared/Header.js
@@ -3,15 +3,24 @@ import React from "react";
 const Header = () => {         
 return (
         <header>
-            <h3>&nbsp;Laaktheater</h3>
-            <nav>
-            <li><a href="/">Home</a></li>
-            <li><a href="/Programma">Programma</a></li>
-            <li><a href="/OverOns">Over ons</a></li>
-            <li><a href="/Contact"><i class="fa-solid fa-basket-shopping"></i></a></li>
-            <li><a href="/Accesibility"><i className="fa fa-wheelchair" /></a></li>
-            </nav>
+            <h3>&nbsp;<a href="/">Laaktheater</a></h3>
+            <nav class="headerClass">
+                <a href="/">Home</a>
+                <a href="/Programma">Programma</a>
+                <a href="/OverOns">Over ons</a>
+                <a href="/Contact"><i class="fa-solid fa-basket-shopping"></i></a>
+                <a href="/Accesibility"><i className="fa fa-wheelchair" /></a>
+                <a class="icon" onclick="buttonClick()"><i class="fa fa-bars"></i></a>
+            </nav>        
         </header>
     );
 } 
+function buttonClick(){
+    var x = document.getElementById("headerLinks");
+    if (x.className === "headerClass") {
+        x.className += " responsive";
+      } else {
+        x.className = "header";
+      }
+}
 export default Header;
