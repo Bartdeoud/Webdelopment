67fe0b0d01012475865ab6a39dc3b82171ed754e
Lucas <LucasClavel356@gmail.com>
2023-01-18 11:07:15 +0100

    cd pipeline fixes

--- frontend/src/Pages/Winkelmand/Winkelmand.js
+++ frontend/src/Pages/Winkelmand/Winkelmand.js
@@ -5,34 +5,35 @@ import Hero2 from "../Shared/Hero2";
 import { useState } from "react";
 
 const Winkelmand = () => {
-    const [totaal, setTotaal] = useState("");
+    // const [totaal, setTotaal] = useState("");
 
-    let handleSubmit = () =>{
-        fetch('https://fakepay.azurewebsites.net/', {
-            method: 'POST',
-            headers:{
-              'Content-Type': 'application/x-www-form-urlencoded'
-            },    
-            body: new URLSearchParams({
-                'amount': totaal,
-                'reference': 'Betaal',
-                'url': 'www.google.com'
-            })
-        });
-    }
+    // let handleSubmit = () =>{
+    //     fetch('https://fakepay.azurewebsites.net/', {
+    //         method: 'POST',
+    //         headers:{
+    //           'Content-Type': 'application/x-www-form-urlencoded'
+    //         },    
+    //         body: new URLSearchParams({
+    //             'amount': totaal,
+    //             'reference': 'Betaal',
+    //             'url': 'www.google.com'
+    //         })
+    //     });
+    // }
 
     return(
         <>
             <Hero2 tekst="Winkelmand"/>
-            <EvenementBlock TitelVoorstelling="Titel Voorstelling" Artiest="artiest" zaal="zaal" datum="datum" tijd="tijd" LinkToImg="..\\assets\\image\\LaakZaal3.jpg"/>
+            {/* <EvenementBlock TitelVoorstelling="Titel Voorstelling" Artiest="artiest" zaal="zaal" datum="datum" tijd="tijd" LinkToImg="..\\assets\\image\\LaakZaal3.jpg"/>
             <section className="contact">
                 <Alinea titel="Dit is een kop" tekst="Dit is een tekst"/>
             </section>
 
             <form onSubmit={handleSubmit}>
-                {/* <input type="number" placeholder="Totaal" onChange={(e) => setTotaal(e.target.value)} /> */}
+                {/* <input type="number" placeholder="Totaal" onChange={(e) => setTotaal(e.target.value)} />
                 <input type="submit" value="Submit" />
-            </form>
+            </form> 
+            */}
         </>
     );
 }
