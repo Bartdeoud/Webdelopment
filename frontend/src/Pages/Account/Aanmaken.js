d32122abdfb671a4c5ef945dfa828c824b4729c9
BarmanTurbo <21035407@student.hhs.nl>
2023-01-19 15:19:48 +0100

    account aanmaken done

--- frontend/src/Pages/Account/Aanmaken.js
+++ frontend/src/Pages/Account/Aanmaken.js
@@ -80,167 +80,130 @@ const Aanmaken = () => {
     return(
         <>
             <Hero2 tekst="Account aanmaken" />
-            <section classname="contact">
-                <p ref={errRef} className={errMsg ? "errmsg" : "offscreen"} aria-live="assertive">{errMsg}</p>
-                <form onSubmit={handleSubmit}>
-                    <label htmlFor="username">
-                        Gebruikersnaam:
-                        <span className={validName ? "valid" : "hide"}>
-                            <FontAwesomeicon icon={faCheck}/>
-                        </span>
-                        <span className={validName || !user ? "hide" : "invalid"}>
-                            <FontAwesomeIcon icon={faTimes}/>
-                        </span>
-                    </label>
-                    <input 
-                        type="text" 
-                        id="username" 
-                        ref={userRef} 
-                        autoComplete="off" 
-                        onChange={(e)=>setUser(e.target.value)} 
-                        required
-                        aria-invalid={validName ? "false" :  "true"}
-                        aria-describedby="uidnote"
-                        onFocus={()=>setUserFocus(true)}
-                        onBlur={()=>setUserFocus(false)}
-                    />
-                    <p id="uidnote" classname={userFocus && user && !validName ? "instructions" : "offscreen"}>
-                        <FontAwesomeicon icon={faInfoCircle}/>
-                        Minimaal 8 karakters
-                        Begint met een letter
-                        Letters, nummers, underscore, streepjes toegestaan.
-                    </p>
+            {success ? (
+                <section>
+                    <h1>Account aanmaken is gelukt!</h1>
+                </section>
+                )
+                : 
+                (
+                    <section classname="contact">
+                        <p ref={errRef} className={errMsg ? "errmsg" : "offscreen"} aria-live="assertive">{errMsg}</p>
+                        <form onSubmit={handleSubmit}>
+                            <label htmlFor="username">
+                                Gebruikersnaam:
+                                <span className={validName ? "valid" : "hide"}>
+                                    <FontAwesomeicon icon={faCheck}/>
+                                </span>
+                                <span className={validName || !user ? "hide" : "invalid"}>
+                                    <FontAwesomeIcon icon={faTimes}/>
+                                </span>
+                            </label>
+                            <input 
+                                type="text" 
+                                id="username" 
+                                ref={userRef} 
+                                autoComplete="off" 
+                                onChange={(e)=>setUser(e.target.value)} 
+                                required
+                                aria-invalid={validName ? "false" :  "true"}
+                                aria-describedby="uidnote"
+                                onFocus={()=>setUserFocus(true)}
+                                onBlur={()=>setUserFocus(false)}
+                            />
+                            <p id="uidnote" classname={userFocus && user && !validName ? "instructions" : "offscreen"}>
+                                <FontAwesomeicon icon={faInfoCircle}/>
+                                Minimaal 8 karakters
+                                Begint met een letter
+                                Letters, nummers, underscore, streepjes toegestaan.
+                            </p>
 
-                    <label htmlFor="emailadres">
-                        E-mail adres:
-                        <span className={validEmail ? "valid" : "hide"}>
-                            <FontAwesomeicon icon={faCheck}/>
-                        </span>
-                        <span className={validName || !user ? "hide" : "invalid"}>
-                            <FontAwesomeIcon icon={faTimes}/>
-                        </span>
-                    </label>
-                    <input 
-                        type="email" 
-                        id="emailadres" 
-                        ref={userRef} 
-                        autoComplete="off" 
-                        onChange={(e)=>setEmail(e.target.value)} 
-                        required
-                        aria-invalid={validName ? "false" :  "true"}
-                        aroa-describedby="emailnote"
-                        onFocus={()=>setEmailFocus(true)}
-                        onBlur={()=>setEmailFocus(false)}
-                    />
-                    <p id="uidnote" classname={userFocus && user && !validName ? "instructions" : "offscreen"}>
-                        <FontAwesomeicon icon={faInfoCircle}/>
-                        Voer een geldig e-mailadres in.
-                    </p>
+                            <label htmlFor="emailadres">
+                                E-mail adres:
+                                <span className={validEmail ? "valid" : "hide"}>
+                                    <FontAwesomeicon icon={faCheck}/>
+                                </span>
+                                <span className={validName || !user ? "hide" : "invalid"}>
+                                    <FontAwesomeIcon icon={faTimes}/>
+                                </span>
+                            </label>
+                            <input 
+                                type="email" 
+                                id="emailadres" 
+                                ref={userRef} 
+                                autoComplete="off" 
+                                onChange={(e)=>setEmail(e.target.value)} 
+                                required
+                                aria-invalid={validName ? "false" :  "true"}
+                                aroa-describedby="emailnote"
+                                onFocus={()=>setEmailFocus(true)}
+                                onBlur={()=>setEmailFocus(false)}
+                            />
+                            <p id="uidnote" classname={userFocus && user && !validName ? "instructions" : "offscreen"}>
+                                <FontAwesomeicon icon={faInfoCircle}/>
+                                Voer een geldig e-mailadres in.
+                            </p>
 
-                    <label htmlFor="password">
-                        Wachtwoord:
-                        <span className={validPwd ? "valid": "hide"}>
-                            <FontAwesomeicon icon={faCheck}/>
-                        </span>
-                        <span classname={validPwd || !password ? "hide" : "invalid"}>
-                            <FontAwesomeicon icon={faTimes}/>
-                        </span>
-                    </label>
-                    <input
-                        type="password"
-                        id="password" 
-                        onChange={(e)=>setPwdFocus(e.target.value)} 
-                        required 
-                        aria-invalid={validPwd ? "false" : "true"}
-                        aria-describedby="pwdnote"
-                        onFocus={()=>setPwdFocus(true)}
-                        onBlur={()=>setPwdFocus(false)}
-                    />
-                    <p id="pwdnote" className={pwdFocus && !validPwd ? "instructions" : "offscreen"}>
-                        <FontAwesomeicon icon={faInfoCircle}/>
-                        Minimaal 7 karakters. <br/>
-                        Bevat minimaal 1 kleine letter, 1 hoofdletter, 1 cijfer en 1 speciaal teken.<br/>
-                        Toegestane speciale tekens: <span aria-label="uitroepteken">!</span> <span aria-label="apenstaartje">@</span> <span aria-label="hekje">#</span> <span aria-label="dollar teken">$</span> <span aria-label="procennt">%</span>
-                    </p>
-                        
-                    <label htmlFor="confirm_pwd">
-                        Bevestig wachtwoord:
-                        <span className={validMatch && confirmPassword ? "valid": "hide"}>
-                            <FontAwesomeicon icon={faCheck}/>
-                        </span>
-                        <span classname={validMatch || !confirmPassword ? "hide" : "invalid"}>
-                            <FontAwesomeicon icon={faTimes}/>
-                        </span>
-                    </label>
-                    <input 
-                        type="password"
-                        id="confirm_pwd"
-                        onChange={(e)=>setMatchPwd(e.target.value)}
-                        required
-                        aria-invalid={validMatch ? "false" : "true"}
-                        aria-describedby="contirmnote"
-                        onFocus={()=>setMatchFocus(true)}
-                        onBlur={()=>setMatchFocus(false)}
-                    />
+                            <label htmlFor="password">
+                                Wachtwoord:
+                                <span className={validPwd ? "valid": "hide"}>
+                                    <FontAwesomeicon icon={faCheck}/>
+                                </span>
+                                <span classname={validPwd || !password ? "hide" : "invalid"}>
+                                    <FontAwesomeicon icon={faTimes}/>
+                                </span>
+                            </label>
+                            <input
+                                type="password"
+                                id="password" 
+                                onChange={(e)=>setPwdFocus(e.target.value)} 
+                                required 
+                                aria-invalid={validPwd ? "false" : "true"}
+                                aria-describedby="pwdnote"
+                                onFocus={()=>setPwdFocus(true)}
+                                onBlur={()=>setPwdFocus(false)}
+                            />
+                            <p id="pwdnote" className={pwdFocus && !validPwd ? "instructions" : "offscreen"}>
+                                <FontAwesomeicon icon={faInfoCircle}/>
+                                Minimaal 7 karakters. <br/>
+                                Bevat minimaal 1 kleine letter, 1 hoofdletter, 1 cijfer en 1 speciaal teken.<br/>
+                                Toegestane speciale tekens: <span aria-label="uitroepteken">!</span> <span aria-label="apenstaartje">@</span> <span aria-label="hekje">#</span> <span aria-label="dollar teken">$</span> <span aria-label="procennt">%</span>
+                            </p>
+                                
+                            <label htmlFor="confirm_pwd">
+                                Bevestig wachtwoord:
+                                <span className={validMatch && confirmPassword ? "valid": "hide"}>
+                                    <FontAwesomeicon icon={faCheck}/>
+                                </span>
+                                <span classname={validMatch || !confirmPassword ? "hide" : "invalid"}>
+                                    <FontAwesomeicon icon={faTimes}/>
+                                </span>
+                            </label>
+                            <input 
+                                type="password"
+                                id="confirm_pwd"
+                                onChange={(e)=>setMatchPwd(e.target.value)}
+                                required
+                                aria-invalid={validMatch ? "false" : "true"}
+                                aria-describedby="contirmnote"
+                                onFocus={()=>setMatchFocus(true)}
+                                onBlur={()=>setMatchFocus(false)}
+                            />
 
-                    <button 
-                        disabled={!validName || !validEmail  || !validPwd || !validMatch ? true : false}
-                    >
-                            Registreer
-                    </button>
-                </form>
-            </section>
+                            <button 
+                                disabled={!validName || !validEmail  || !validPwd || !validMatch ? true : false}
+                            >
+                                    Registreer
+                            </button>
+                        </form>
+                    </section>
+                )
+            }
         </>
     )
-    /*return (
-        <>
-            <Hero2 tekst="Account aanmaken" />
-                
-            <section className="contact">
-             <form>
-                <p>*verplicht</p>
-                <label htmlFor="Username">*Gebruikersnaam:</label>
-                <input type="text" onChange={e => setNaam(e.target.value)} id="Username" name="Username" required/>
-                <br/>
-                <label htmlFor="Wachtwoord">*Wachtwoord:</label>
-                <input type="password" pattern="(?=.*[a-z])(?=.*[A-Z])(?=.*[$&+,:;=?@#|'<>.^*()%!-]).{7,}" onChange={e => setPassword(e.target.value)} id="Wachtwoord" name="Wachtwoord" required/>
-                <br/>
-                <label htmlFor="herhaalWachtwoord">*Bevestig wachtwoord:</label>
-                <input type="password" onChange={e => setConfirmPassword(e.target.value)} id="herhaalWachtwoord" name="herhaalWachtwoord" required/>
-
-                <PasswordChecklist
-                    //these are the default rules
-                    rules={["minLength","specialChar","number","capital","lowercase","match"]}
-                    minLength={7}
-                    capital={1}
-                    lowercase={1}
-                    specialChar={1}
-                    value={password}
-                    valueAgain={confirmPassword}
-                    messages={{
-                        // this changes the default messages
-                        minLength: "Minimaal 7 karakters",
-                        specialChar: "Minimaal 1 speciaal karakter",
-                        number: "Minimaal 1 getal",
-                        capital: "Minimaal 1 hoofdletter",
-                        lowercase: "Minimaal 1 kleine letter",
-                        match: "Wachtwoorden moeten overeen komen",
-                    }}
-                    onChange={(isValid) => {}}
-			    />
-                <br></br>
-
-                <p>*Email</p>
-                <input type="email" onChange={e => setEmail(e.target.value)} required/>
-                
-                <p>Geboortedatum</p>
-                <input type="date" onChange={e => setDate(e.target.value)} />
-
-                <button className="btn" onClick={handleSubmit}> Submit </button>
-            </form>
-            </section>   
-        </>
-    );*/
+        
+    
+    
 }
 
 export default Aanmaken;
