6f1f5087d7cb83035da0875c030ef32caca3bb38
Profnoobstone <bartdeoud@gmail.com>
2023-01-19 19:24:01 +0100

    usestate

--- frontend/src/DataFetching.js
+++ frontend/src/DataFetching.js
@@ -1,6 +1,5 @@
 import React, {useEffect, useState} from "react";
 import axios from "axios";
-import { useState } from "react";
 
 function DataFetching(){
     const [posts, setPosts] = useState([])
