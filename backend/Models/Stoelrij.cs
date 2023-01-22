32a5aa8522dd82761abde3d50d8658c9778b6abd
BarmanTurbo <21035407@student.hhs.nl>
2023-01-22 14:08:27 +0100

    key fix

--- backend/Models/Stoelrij.cs
+++ backend/Models/Stoelrij.cs
@@ -1,4 +1,8 @@
+using System.ComponentModel.DataAnnotations;
 public class Stoelrij{
+
+    [Key]
+    public int rijid{get;set;}
     public int Rangnummer{get;set;}
 
     public int Aantal_stoelen{get;set;}=0;
