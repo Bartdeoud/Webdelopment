ffe9e19e2ddb26574db1b2890ea256079c7283c5
BarmanTurbo <21035407@student.hhs.nl>
2023-01-22 14:34:59 +0100

    uitlijning

--- backend/functions/DBContext.cs
+++ backend/functions/DBContext.cs
@@ -30,14 +30,14 @@ public class DBContext : DbContext
             new Gebruiker(){ Email = "artiestmail5@email.com", Username="Artiest5", Naam = "Artiest5", Wachtwoord = "Test5", UserID = 9}
         );
         builder.Entity<Stoelrij>().HasData(
-            new Stoelrij(){Rangnummer=1,Aantal_stoelen=20, rijid=11, Zaalnr=1},
+            new Stoelrij(){Rangnummer=1,Aantal_stoelen=20,  rijid=11, Zaalnr=1},
             new Stoelrij(){Rangnummer=2,Aantal_stoelen=100, rijid=12, Zaalnr=1},
             new Stoelrij(){Rangnummer=3,Aantal_stoelen=120, rijid=13, Zaalnr=1},
-            new Stoelrij(){Rangnummer=1,Aantal_stoelen=2, rijid=21, Zaalnr=2},
+            new Stoelrij(){Rangnummer=1,Aantal_stoelen=2,   rijid=21, Zaalnr=2},
             new Stoelrij(){Rangnummer=2,Aantal_stoelen=160, rijid=22,Zaalnr=2},
-            new Stoelrij(){Rangnummer=1,Aantal_stoelen=10, rijid=31, Zaalnr=3},
-            new Stoelrij(){Rangnummer=2,Aantal_stoelen=80, rijid=32, Zaalnr=3},
-            new Stoelrij(){Rangnummer=1,Aantal_stoelen=40, rijid=41, Zaalnr=4},
+            new Stoelrij(){Rangnummer=1,Aantal_stoelen=10,  rijid=31, Zaalnr=3},
+            new Stoelrij(){Rangnummer=2,Aantal_stoelen=80,  rijid=32, Zaalnr=3},
+            new Stoelrij(){Rangnummer=1,Aantal_stoelen=40,  rijid=41, Zaalnr=4},
             new Stoelrij(){Rangnummer=2,Aantal_stoelen=200, rijid=42, Zaalnr=4},
             new Stoelrij(){Rangnummer=3,Aantal_stoelen=200, rijid=43, Zaalnr=4}
         );
