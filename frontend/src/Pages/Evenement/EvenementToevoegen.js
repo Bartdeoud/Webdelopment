a89dc59340281517505ae1c6235da18ea79716f3
Kikkerslijm410 <Lucasclavel356@gmail.com>
2023-01-22 23:26:06 +0100

    fixes so azure doesnt complain

--- frontend/src/Pages/Evenement/EvenementToevoegen.js
+++ frontend/src/Pages/Evenement/EvenementToevoegen.js
@@ -1,56 +1,61 @@
 import React from 'react';
 import Hero2 from '../Shared/Hero2';
-import { useState } from "react";
+// import { useState } from "react";
 // import { DayPicker } from "react-day-picker";
 import 'react-day-picker/dist/style.css';
-import axios from 'axios';
+// import axios from 'axios';
 
 
 const EvenementToevoegen = () => {
     // const [day, setDay] = useState();
-    const [shownaam, setNaam] = useState();
-    const [showzaal, setZaal] = useState();
-    const [leeftijd, setLeeftijd] = useState();
-    const [showgenre, setGenre] = useState();
+    // const [shownaam, setNaam] = useState();
+    // const [showzaal, setZaal] = useState();
+    // const [leeftijd, setLeeftijd] = useState();
+    // const [showgenre, setGenre] = useState();
 
     const handleSubmit = (e) => {
-        handleOnSubmit();
+      handleOnSubmit();
     }
 
-    const handleOnSubmit2 = async (e) => {
-      axios.post("https://localhost:7214/api/Show/ShowToevoegen", {
-        shownr: 0,
-        afbeelding: "string",
-        genre: "showgenre",
-        naam: "shownaam",
-        leeftijdsgroep: "leeftijd",
-        zaal: "showzaal",
-        beginTijd: "2023-01-22T22:09:07.168Z",
-        eindTijd: "2023-01-22T22:09:07.168Z"
-      })
-      .then(res => {
-        console.log(res);
-        console.log(res.data);
-      })
-    }
+    // const handleOnSubmit2 = async (e) => {
+    //   axios.post("https://localhost:7214/api/Show/ShowToevoegen", {
+    //     shownr: 0,
+    //     afbeelding: "string",
+    //     genre: "showgenre",
+    //     naam: "shownaam",
+    //     leeftijdsgroep: "leeftijd",
+    //     zaal: "showzaal",
+    //     beginTijd: "2023-01-22T22:09:07.168Z",
+    //     eindTijd: "2023-01-22T22:09:07.168Z"
+    //   })
+    //   .then(res => {
+    //     console.log(res);
+    //     console.log(res.data);
+    //   })
+    // }
 
 
-    let handleOnSubmit = async (e) => {
+    const handleOnSubmit = async () => {
         try {
-          axios.post("https://localhost:7214/api/Show/ShowToevoegen", {
-            shownr: 0,
-            afbeelding: "string",
-            genre: "showgenre",
-            naam: "shownaam",
-            leeftijdsgroep: "leeftijd",
-            zaal: "showzaal",
-            beginTijd: "2023-01-22T22:09:07.168Z",
-            eindTijd: "2023-01-22T22:09:07.168Z"
-          })
-          .then(res => {
-            console.log(res);
-            console.log(res.data);
-          })
+          let res = await fetch("https://localhost:7214/api/Show/ShowToevoegen", {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+              shownr: 0,
+              afbeelding: "string",
+              genre: "showgenre",
+              naam: "shownaam",
+              leeftijdsgroep: "leeftijd",
+              zaal: "showzaal",
+              beginTijd: "2023-01-22T22:09:07.168Z",
+              eindTijd: "2023-01-22T22:09:07.168Z"
+            }),
+          });
+          if (res.status === 200) {
+            console.log("succes");
+          }
         } catch (err) {
           console.log(err);
         }
@@ -62,7 +67,7 @@ const EvenementToevoegen = () => {
             <section className="contact">
                 <form>
                     <p>Naam evenement</p>
-                    <input type="text" placeholder="Voer hier de naam van de show in" onChange={(e)=>setNaam(e.target.value)}/>
+                    {/* <input type="text" placeholder="Voer hier de naam van de show in" onChange={(e)=>setNaam(e.target.value)}/> */}
 
                     {/* <p>Zaal</p>
                     <select required="required"  onChange={(e)=>setZaal(e.target.value)}>
