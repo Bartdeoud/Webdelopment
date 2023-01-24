8c962abf54a751d085ecc6875ac4134eb6a852a7
Kikkerslijm410 <Lucasclavel356@gmail.com>
2023-01-24 10:58:12 +0100

    commented all the login shit

--- backend/Program.cs
+++ backend/Program.cs
@@ -20,39 +20,43 @@ builder.Services.AddCors(options => {
     });
 });
 
-builder.Services.AddIdentity<IdentityUser, IdentityRole>()
-                .AddEntityFrameworkStores<DBContext>()
-                .AddDefaultTokenProviders();
+// builder.Services.AddIdentity<IdentityUser, IdentityRole>()
+//                 .AddEntityFrameworkStores<DBContext>()
+//                 .AddDefaultTokenProviders();
 
-builder.Services.AddAuthentication(opt =>
-{
-    opt.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
-    opt.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
-}).AddJwtBearer(opt =>
-{
-    opt.TokenValidationParameters = new Microsoft.IdentityModel.Tokens.TokenValidationParameters
-    {
-        ValidateIssuer = true,
-        ValidateAudience = true,
-        ValidateLifetime = true,
-        ValidateIssuerSigningKey = true,
-        ValidIssuer = "https://localhost:7047",
-        ValidAudience = "https://localhost:7047",
-        IssuerSigningKey = new SymmetricSecurityKey(System.Text.Encoding.UTF8.GetBytes("awef98awef978haweof8g7aw789efhh789awef8h9awh89efh89awe98f89uawef9j8aw89hefawef"))
-    };
-});
+// builder.Services.AddAuthentication(opt =>
+// {
+//     opt.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+//     opt.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+// }).AddJwtBearer(opt =>
+// {
+//     opt.TokenValidationParameters = new Microsoft.IdentityModel.Tokens.TokenValidationParameters
+//     {
+//         ValidateIssuer = true,
+//         ValidateAudience = true,
+//         ValidateLifetime = true,
+//         ValidateIssuerSigningKey = true,
+//         ValidIssuer = "https://localhost:7047",
+//         ValidAudience = "https://localhost:7047",
+//         IssuerSigningKey = new SymmetricSecurityKey(System.Text.Encoding.UTF8.GetBytes("awef98awef978haweof8g7aw789efhh789awef8h9awh89efh89awe98f89uawef9j8aw89hefawef"))
+//     };
+// });
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen(c => c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme{
-    Name = "Authorization",
-    Type = SecuritySchemeType.ApiKey,
-    Scheme = "Bearer",
-    BearerFormat = "JWT",
-    In = ParameterLocation.Header,
-    Description = "JWT Authorization header using the Bearer scheme."
-}));
+
+builder.Services.AddSwaggerGen();
+
+// builder.Services.AddSwaggerGen(c => c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme{
+//     Name = "Authorization",
+//     Type = SecuritySchemeType.ApiKey,
+//     Scheme = "Bearer",
+//     BearerFormat = "JWT",
+//     In = ParameterLocation.Header,
+//     Description = "JWT Authorization header using the Bearer scheme."
+// }));
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -64,7 +68,7 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-app.UseAuthentication();
+// app.UseAuthentication();
 
 app.UseAuthorization();
 
