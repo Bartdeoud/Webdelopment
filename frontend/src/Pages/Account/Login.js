5b2282117729604ae0c953a275eecb7ee3f951b5
Kikkerslijm410 <Lucasclavel356@gmail.com>
2023-01-25 12:14:10 +0100

    Bugfixes and cleanup

--- frontend/src/Pages/Account/Login.js
+++ frontend/src/Pages/Account/Login.js
@@ -1,37 +1,115 @@
-import React, { useState } from "react";
-import Hero2 from "../Shared/Hero2";
+import React from 'react';
+import Hero2 from '../Shared/Hero2';
+import Alinea from '../Shared/Alinea';
+import { useRef, useState, useEffect, useContext} from "react";
+import AuthContext from "../../context/AuthProvider";
+import axios from "../../api/axios";
 
+const LOGIN_URL = "/auth";
 
-const Login = () => {
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
-    
+const Login = (props) => {
+    const { setAuth } = useContext(AuthContext);
 
-    const handleSubmit = (e) => {
-        e.preventDefault();
+    //sets focus on error message for screenreaders
+    const userRef = useRef();
+    const errRef = useRef();
 
-        alert(email + password);
+    const [gebruikersnaam, setGebruikersnaam] = useState("");
+    const [wachtwoord, setWachtwoord]=useState("");
+    const [formData, setFormData]=useState("");
+
+    const [errMsg, setErrMsg] = useState("");
+    const [success, setSuccess] = useState(false);
+
+    //This is just here to prevent warnings since azure doesn't like unused variables
+    const placeholder = () => {
+        console.log (setWachtwoord);
+        console.log (setGebruikersnaam);
+        console.log (formData);
+        console.log (setFormData);
     }
 
-    return(
+    useEffect(()=>{
+        userRef.current.focus();
+    },[]);
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        console.log (placeholder);
+
+        try{
+            const response = await axios.post(LOGIN_URL,
+                JSON.stringify({user: gebruikersnaam,pwd:wachtwoord}),
+                {
+                    headers: {'Content-Type': 'application/json'},
+                    withCredentials: true
+                }
+            );
+            console.log(JSON.stringify(response?.data));
+            const accessToken = response?.data?.accessToken;
+            const roles = response?.data?.roles;
+            setAuth({gebruikersnaam, wachtwoord, roles, accessToken});
+            setGebruikersnaam("");
+            setWachtwoord("");
+            setSuccess(true);   
+        }catch(err){
+            if(!err?.response){
+                setErrMsg('No Server Response')
+            }else{
+                if(err.response?.status === 400){
+                    setErrMsg('Gebruikersnaam of wachtwoord ontbreekt');
+                }else{
+                    setErrMsg('Login lukte niet');
+                }
+                errRef.current.focus();
+            }
+        }
+
+        console.log(gebruikersnaam);
+        console.log(wachtwoord);
+    }
+
+    return (
         <>
-            <Hero2 tekst = "Inloggen"/>
-            <section className="section">
-                <form onSubmit={handleSubmit}>
-                    <label>
-                        Email:
-                        <input type="text" id="Email" onChange={(e) => setEmail(e.target.value)}/>
-                    </label>
-                    <br/><br/>
-                    <label>
-                        Wachtwoord:
-                        <input type="text" id="Wachtwoord" onChange={(e) => setPassword(e.target.value)}/>
-                    </label>
-                    <br/><br/>
-                    <button className="btn" type="submit">Login</button>
-                    <br/><br/>
-                </form>
-            </section>
+            {success ? (
+                <>
+                    <Hero2 tekst = "Inloggen"/>
+                    <Alinea titel="U bent ingelogd" tekst="U bent al ingelogd"/>
+                </>
+            ):(
+                <>
+                    <Hero2 tekst="Inloggen" />
+                        
+                    <section className="contact">
+                        <p ref={errRef} className={errMsg?"errmsg":"offscreen"} aria-live="assertive">{errMsg}</p>
+                        <form onSubmit={handleSubmit}>
+                            <label htmlFor="gebruikersnaam">Gebruikersnaam:</label>
+                            <input
+                                value={gebruikersnaam}
+                                type="text"
+                                ref={userRef}
+                                placeholder={props.tekst}
+                                onChange={(e)=> setGebruikersnaam(e.target.value)}
+                                id="gebruikersnaam"
+                                name="gebruikersnaam"
+                                required
+                            />
+                            <br/>
+                            <label htmlFor="wachtwoord">Wachtwoord:</label>
+                            <input
+                                value={wachtwoord}
+                                type="password"
+                                placeholder={props.tekst}
+                                onChange={(e)=>setWachtwoord(e.target.value)}
+                                id="wachtwoord"
+                                name="wachtwoord"
+                                required
+                            />
+                            <button className="btn" onClick={handleSubmit} type="submit">Log in</button>
+                        </form>
+                    </section>
+                </>
+            )} 
         </>
     );
 }
--- frontend/src/Pages/Winkelmand/Huren.js
+++ frontend/src/Pages/Winkelmand/Huren.js
@@ -1,42 +1,35 @@
 import React, { useState }  from 'react';
 import Hero2 from '../Shared/Hero2';
-// import { DayPicker } from "react-day-picker";
-import 'react-day-picker/dist/style.css';
 
 const Huren = () => {
-    // const [day, setDay] = useState();
     const [hurenNaam, setNaam] = useState();
     //const [userID, getID] = useState();
     const [hurenZaal, setZaal] = useState();
     const [hurenRuimte, setRuimte] = useState();
-
-    const handleSubmit = (e) => {
-        handleOnSubmit();
-      }
   
-      const handleOnSubmit = async () => {
-          try {
-            let res = await fetch("https://localhost:7214/api/Huren", {
-              method: "POST",
-              headers: {
-                "Content-Type": "application/json",
-              },
-              body: JSON.stringify({
-                id: 0,
-                naam: hurenNaam,
-                datum:  "2023-01-22T22:09:07.168Z",
-                verhuurdeId: 0,
-                zaalnr: hurenZaal,
-                ruimteNr: hurenRuimte,
-              }),
-            });
-            if (res.status === 200) {
-              console.log("succes");
-            }
-          } catch (err) {
-            console.log(err);
-          }
+    const handleSubmit = async () => {
+        try {
+        let res = await fetch("https://localhost:7214/api/Huren", {
+            method: "POST",
+            headers: {
+            "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+            id: 0,
+            naam: hurenNaam,
+            datum:  "2023-01-22T22:09:07.168Z",
+            verhuurdeId: 0,
+            zaalnr: hurenZaal,
+            ruimteNr: hurenRuimte,
+            }),
+        });
+        if (res.status === 200) {
+            console.log("succes");
         }
+        } catch (err) {
+        console.log(err);
+        }
+    }
   
     try{
         //cookies users
@@ -72,34 +65,24 @@ const Huren = () => {
                             <option value="ruimte8">Ruimte 8</option>
                             <option value="ruimte9">Ruimte 9</option>
                             <option value="ruimte10">Ruimte 10</option>
-    
                         </select>
-    
+
                         <br/>
-    
-                        {/* <p>Dagen</p>
-                        <DayPicker
-                            mode="single"
-                            selected={day}
-                            onSelect={setDay}
-                        /> */}
-    
                         <button className="btn" onClick={handleSubmit}> Submit </button>
                     </form>
                 </section>   
             </>
         );
-
     }catch{
         return(
             <>
                 <Hero2 tekst="Huren"/>
-                <Alinea titel="Uw moet eerst inloggen om een zaal te huren." link="/Account" linknaam="Log in om te kunnen huren."/>
+                <Alinea titel="Uw moet eerst inloggen om een zaal te huren." 
+                link="/Account" 
+                linknaam="Log in om te kunnen huren."/>
             </>
         );
     }
-
-    
 }
 
 export default Huren;
