620943c7891129f9e01325a5f950322490473d0a
Kikkerslijm410 <Lucasclavel356@gmail.com>
2023-01-26 14:34:40 +0100

    Bart check please

--- backend/Models/Show.cs
+++ backend/Models/Show.cs
@@ -1,12 +1,18 @@
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
 public class Show
 {
     [Key]
     public int Shownr { get; set; }
     public string? Afbeelding { get; set; }
-    public string? Genre { get; set; }
+
+    [ForeignKey("Genre")]
+    public int? Genre { get; set; }
     public string Naam { get; set; } = "";
-    public string? Leeftijdsgroep { get; set; }
+
+    [ForeignKey("Leeftijdsgroep")]
+    public int? Leeftijdsgroep { get; set; }
     public string? zaal{get; set;}
     public DateTime BeginTijd { get; set; }
     public DateTime EindTijd { get; set; }
--- backend/Models/Zaal.cs
+++ backend/Models/Zaal.cs
@@ -4,6 +4,9 @@ public class Zaal {
     [Key]
     public int Zaalnr {get;set;}
     public string Naam {get;set;}
-    public int Aantal_stoelen {get;set;}
-    
+    public int Rang1 {get;set;}
+    public int? Rang2 {get;set;}
+    public int? Rang3 {get;set;}
+    public int? Rang4 {get;set;}
+    public int? invalideplaatsen {get;set;}
 }
