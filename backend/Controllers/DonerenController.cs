0ec424c304f3f73187946d7497a21197fd5badbb
Profnoobstone <bartdeoud@gmail.com>
2023-01-31 16:09:22 +0100

    Doneren

--- backend/Controllers/DonerenController.cs
+++ backend/Controllers/DonerenController.cs
@@ -1,4 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
+using System.Text;
+using System.Text.Json;
 
 namespace api.Controllers;
 
@@ -13,9 +15,41 @@ public class DonerenController : ControllerBase
 
     //POST api/<PayController>
     [HttpPost]
-    public IActionResult Post([FromForm] string token)
+    public async Task<IActionResult> Post(string bedrag , [FromForm] string token)
     {
-        Console.WriteLine(token);
-        return Redirect("http://localhost:3000/Doneren??token=" + token);
+
+        Console.WriteLine(bedrag);
+
+        using StringContent jsonContent = new(
+        JsonSerializer.Serialize(new
+        {
+            Doel = 74,
+            Hoeveelheid = bedrag,
+            Tekst = "Donatie aan SIM"
+        }),
+        Encoding.UTF8,
+        "application/json");
+
+        HttpClient client = new HttpClient();
+
+        client.DefaultRequestHeaders.Add("Authorization", "Bearer " + token);
+
+        var response = await client.PostAsync("https://ikdoneer.azurewebsites.net/api/donatie", jsonContent);
+
+        var responseString = await response.Content.ReadAsStringAsync();
+
+        Console.WriteLine(responseString);
+
+        return Redirect("http://localhost:3000/Doneren?");
+    }
+}
+
+public class values{
+    public string? header{get;set;}
+    public string? value{get;set;}
+
+    public values(string header, string value){
+        this.header = header;
+        this.value = value;
     }
 }
