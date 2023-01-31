e47fc802df368cf8a3b207a99f1535ff57462fef
Kikkerslijm410 <Lucasclavel356@gmail.com>
2023-01-31 11:26:37 +0100

    oopsie poopsie

--- frontend/cypress/e2e/AltTest.cy.js
+++ frontend/cypress/e2e/AltTest.cy.js
@@ -7,20 +7,20 @@ describe('Alternate text test', () => {
     })
 
     it('Check if alt text is displayed correctly (Hero2)', () => {
-        cy.visit('https://salmon-smoke-00d5f3d03.2.azurestaticapps.net/')
+        cy.visit('https://salmon-smoke-00d5f3d03.2.azurestaticapps.net/Programma')
         cy.get('.hero2')
             .find('div')
             .should('have.attr', 'alt', 'Theater doek als achtergrond')
     })
 
     it('Check if alt text is displayed correctly (Programma)', () => {
-        cy.visit('https://salmon-smoke-00d5f3d03.2.azurestaticapps.net/')
+        cy.visit('https://salmon-smoke-00d5f3d03.2.azurestaticapps.net/Programma')
         cy.get(':nth-child(2) > .evenementblock > :nth-child(1) > img')
             .should('have.attr', 'alt', 'Afbeelding show')
     })
 
     it('Check if alt text is displayed correctly (Show)', () => {
-        cy.visit('https://salmon-smoke-00d5f3d03.2.azurestaticapps.net/')
+        cy.visit('https://salmon-smoke-00d5f3d03.2.azurestaticapps.net/Programma')
         cy.contains('bestel').click()
         cy.get('img')
             .should('have.attr', 'alt', 'Afbeelding show')    
